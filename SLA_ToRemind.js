007b6956b81685cc3f8b71c70b989d66401cf8da
SRGLAWS <serge.lawson@gmail.com>
2016-07-06 15:49:18 +0200

    Add files via upload

---
+++ SLA_ToRemind.js
@@ -0,0 +1,82 @@
+**** SONS ****
+Jo cocker
+Carlos Santana
+Eric Bellinger-Creep
+Waited 4 u (Odesza remix) - Slow magic
+Freddie gibbs - Dope house
+Leo stannard  -  please don't
+Alessia Cara -  Here
+Ad-2 - Bomber & A fly chick
+Whitney Houston - I have nothing
+Alphaville -  forever young
+Macy grey - Mercy
+Denai moore
+Neil Young
+Raphaël
+Checkh lo
+Ejimi les jumelles river
+The pretenders go to sleep
+
+**** FILMS ****
+L'enfant 44
+The dubbler
+Requiem for a dream
+Trainspotting
+Enter the void
+L'armée des 12 singes
+The dead man show
+Birdman
+Matrix
+Beasts of no nations
+Dope, 
+NWA, 
+Train sputting,  
+Requiem for a dream
+
+**** SERIES ****
+Aquarius
+True detective
+Heroes suite
+Broadwalk empire
+Dracula suite
+Hannibal lecter
+Penny dreadful
+Vikings suite 
+Marco polo
+Braquo
+Bureau des légendes
+Engrenages
+hemlock grove
+Saint eight, 
+Narco, 
+Daredevil,
+Boardwalk empire, 
+Hannibal, 
+Banshee, 
+Kingdom, 
+Ray donovan, 
+Vikings, Marco Polo, Dracula
+
+Inherence vice
+Snow in the Paradise
+Snow on the bluff 
+Une nuit en enfer
+Old boy
+
+Mad men
+House of cards
+The extant
+The following
+Black heals
+
+**** DIVERS ****
+Code WU: Friends1506
+Diesel DZ4227
+Diesel DZ4291
+Couche pour tous.com
+
+Serge-- Loyer, mensualité prêt, assu voiture, réparations voiture, internet, assu habitation
+Tina-- bouffe, EDF
+
+
+Dev pr Max et Tim -> Lecture videos
